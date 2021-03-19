c16893b3211b54dc66e311ccb28ac4bea0b3148d
Kai Li <kal182@ucsd.edu>
2021-03-18 20:36:35 -0700

    decode token middleware

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import cors from 'cors';
 import swagger from './swagger';
 import teams from './routes/teams';
 import users from './routes/users';
+import {decodeAuthToken} from './utils/middlewares';
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(decodeAuthToken);
 app.use(cors());
 
 // routes
