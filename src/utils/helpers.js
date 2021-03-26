d8690a881a44f491eea6a4ec9f8952ef4d94b8cf
Kai Li <kal182@ucsd.edu>
2021-03-26 12:03:20 -0700

    change s3 implementation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,11 @@
 import UserInTeamModel from '../models/userInTeam';
-import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
-import {S3Client, GetObjectCommand, PutObjectCommand} from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+  region: 'us-east-2',
+  signatureVersion: 'v4',
+});
+const bucket = 'standup-master-dev';
 
 const checkTwoUsersInSameTeam = async (userEmail1, userEmail2) => {
   const userInTeam1 = await UserInTeamModel.get(userEmail1);
@@ -14,25 +19,20 @@ const checkTwoUsersInSameTeam = async (userEmail1, userEmail2) => {
 };
 
 const generateSignedUrlForProfilePicture = async (email) => {
-  const client = new S3Client({region: 'us-east-2'});
-  const command = new GetObjectCommand({
-    Bucket: 'standup-master-dev',
-    Key: `profile-pictures/${email}`,
-    Region: 'us-east-2',
+  const url = await s3.getSignedUrl('getObject', {
+    Bucket: bucket,
+    Key: `profile-pictures/${email}/picture`,
+    Expires: 24 * 60 * 60,
   });
-  const url = await getSignedUrl(client, command, {expiresIn: 24 * 60 * 60}); // expire in 1 day
   return url;
 };
 
 const uploadProfilePicture = async (email, picture) => {
-  const client = new S3Client({region: 'us-east-2'});
-  const command = new PutObjectCommand({
-    Bucket: 'standup-master-dev',
-    Key: `profile-pictures/${email}`,
-    Region: 'us-east-2',
+  await s3.putObject({
+    Bucket: bucket,
+    Key: `profile-pictures/${email}/picture`,
     Body: picture,
-  });
-  await client.send(command);
+  }).promise();
 };
 
 export {
