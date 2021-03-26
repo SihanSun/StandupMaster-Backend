a1cec04fad14255e9b9070447dad74e05392a30d
Kai Li <kal182@ucsd.edu>
2021-03-26 10:54:53 -0700

    upload and get profile image

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,6 @@
 import UserInTeamModel from '../models/userInTeam';
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
+import {S3Client, GetObjectCommand, PutObjectCommand} from '@aws-sdk/client-s3';
 
 const checkTwoUsersInSameTeam = async (userEmail1, userEmail2) => {
   const userInTeam1 = await UserInTeamModel.get(userEmail1);
@@ -11,4 +13,30 @@ const checkTwoUsersInSameTeam = async (userEmail1, userEmail2) => {
   return true;
 };
 
-export {checkTwoUsersInSameTeam};
+const generateSignedUrlForProfilePicture = async (email) => {
+  const client = new S3Client({region: 'us-east-2'});
+  const command = new GetObjectCommand({
+    Bucket: 'standup-master-dev',
+    Key: `profile-pictures/${email}`,
+    Region: 'us-east-2',
+  });
+  const url = await getSignedUrl(client, command, {expiresIn: 24 * 60 * 60}); // expire in 1 day
+  return url;
+};
+
+const uploadProfilePicture = async (email, picture) => {
+  const client = new S3Client({region: 'us-east-2'});
+  const command = new PutObjectCommand({
+    Bucket: 'standup-master-dev',
+    Key: `profile-pictures/${email}`,
+    Region: 'us-east-2',
+    Body: picture,
+  });
+  await client.send(command);
+};
+
+export {
+  checkTwoUsersInSameTeam,
+  generateSignedUrlForProfilePicture,
+  uploadProfilePicture,
+};
